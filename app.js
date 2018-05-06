51f08e34333a2cefbb88be3e4854fab3fbdb0a3f
pankajsy9 <pankaj.sy9@gmail.com>
2018-05-06 16:44:47 +0530

    static-favicon entry removed.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express'),
 var https = require('https');
 var fs = require('fs');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -49,7 +48,6 @@ app.set('view engine', 'handlebars');
 
 //app.set('view engine', 'jade');
 
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
