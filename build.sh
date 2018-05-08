b3cb624fdd6e8ba8e022da92152ef4b2cfd78523
pankajsy9 <pankaj.sy9@gmail.com>
2018-05-08 16:42:12 +0530

    Handlebar compilation not required.

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo "Compiling handlebars partials"
-handlebars views/partials/ -f public/assets/js/hbpartials.js
-
 echo "Browserifying wallet.js"
 browserify public/assets/js/wallet.js -o public/assets/js/walletbundle.js
